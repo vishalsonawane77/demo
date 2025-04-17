383bcba583b78b5f54866a37c9bbf92f43159988
Vishal Sonawane <157272188+vishalsonawane77@users.noreply.github.com>
2025-04-17 21:05:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def predict():
         return f"Error: {str(e)}", 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
